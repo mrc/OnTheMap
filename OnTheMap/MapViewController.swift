f11b856b99bcad1874aff71d139743813c69a969
Matt Curtis <matt.r.curtis@gmail.com>
2015-07-26 22:34:28 +1000

    Show web page when a pin annotation is clicked.

--- OnTheMap/MapViewController.swift
+++ OnTheMap/MapViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MapKit
 
-class MapViewController: StudentLocationsViewController {
+class MapViewController: StudentLocationsViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
 
@@ -30,9 +30,31 @@ class MapViewController: StudentLocationsViewController {
                     let coords = CLLocationCoordinate2DMake(latitude, longitude)
                     let title = "\(firstName) \(lastName)"
                     let pin = StudentLocationPin(coordinate: coords, title: title, subtitle: url)
+                    pin
                     mapView.addAnnotation(pin)
             }
         }
+    }
+
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+
+        let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pin")
+        view.canShowCallout = true
+        view.rightCalloutAccessoryView = UIButton.buttonWithType(UIButtonType.DetailDisclosure) as! UIView
+        return view
+    }
+
+    func mapView(mapView: MKMapView!, annotationView view: MKAnnotationView!, calloutAccessoryControlTapped control: UIControl!) {
+
+        if let urlString = view.annotation.subtitle,
+            url = NSURL(string: urlString)
+            where UIApplication.sharedApplication().canOpenURL(url) {
+                UIApplication.sharedApplication().openURL(url)
+        }
+    }
+
+    func mapView(mapView: MKMapView!, didSelectAnnotationView view: MKAnnotationView!) {
+
     }
 
 }
